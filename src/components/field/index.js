5736ebb0e0918418d00c29cc87c0f9750ccb7713
AntoineDautreville <antoine.dautreville@gmail.com>
2021-09-03 10:47:05 +0200

    Use native driver false

--- src/components/field/index.js
+++ src/components/field/index.js
@@ -230,7 +230,7 @@ export default class TextField extends PureComponent {
     let options = {
       toValue: this.focusState(),
       duration,
-      useNativeDriver: true,
+      useNativeDriver: false,
     };
 
     startAnimation(focusAnimation, options, this.onFocusAnimationEnd);
@@ -242,7 +242,7 @@ export default class TextField extends PureComponent {
 
     let options = {
       toValue: this.labelState(),
-      useNativeDriver: true,
+      useNativeDriver: false,
       duration,
     };
 
